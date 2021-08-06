031bf0ed239c3455572b4c505880584f10010781
bje0716 <byon0716@gmail.com>
2021-08-06 23:37:49 +0900

    remove c file

--- person.c
+++
@@ -1,5 +0,0 @@
-struct Person {
-	char name[20]; // 이름
-	char tel[15]; // 연락처
-	int latePaymentCount; // 연체횟수
-};
--- rentalinfo.c
+++
@@ -1,7 +0,0 @@
-struct RentalInfo {
-	char dvdName[100]; // dvd 이름
-	char rentalName[20]; // 대여자
-	int rentalDate; // 대여날짜
-	int returnDate; // 반납날짜
-	bool isLatePayment; // 연체여부
-};
