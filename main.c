30282e429574c74bf8468ac082a4d445967c4374
bje0716 <byon0716@gmail.com>
2021-08-08 03:34:32 +0900

    기능 추가
    - 회원추가 기능
    - 회원출력 기능
    - 그 외 메인창 출력

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 // 매크로 상수 선언
 #define ARR_SIZE 100
 #define STR_LEN 20
-#define TEL_LEN 15
+#define TEL_LEN 20
 #define ISBN_LEN 50
 #define DVD_NAME_LEN 100
 
@@ -14,6 +14,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
+#include <Windows.h>
 
 // 구조체 선언 부분
 typedef struct {
@@ -41,6 +42,7 @@ typedef struct {
 	int latePaymentCount; // 연체횟수
 } BlackList;
 
+// 열거형 선언 부분
 enum {
 	// 메인 메뉴
 	PERSON_INFO = 1,
@@ -52,106 +54,328 @@ enum {
 
 	// 회원관리 메뉴
 	PERSON_ADD = 1,
-	PERSON_RENEWAL = 2,
-	PERSON_DELETE = 3,
-	PERSON_MENU_RETURN = 4,
-	PERSON_QUIT = 5,
+	PERSON_PRINT = 2,
+	PERSON_RENEWAL = 3,
+	PERSON_DELETE = 4,
+	PERSON_MENU_RETURN = 5,
+	PERSON_QUIT = 6,
 
 	// DVD 정보관리 메뉴
 	DVD_ADD = 1,
-	DVD_RENEWAL = 2,
-	DVD_DELETE = 3,
-	DVD_MENU_RETURN = 4,
-	DVD_QUIT = 5
+	DVD_PRINT = 2,
+	DVD_RENEWAL = 3,
+	DVD_DELETE = 4,
+	DVD_MENU_RETURN = 5,
+	DVD_QUIT = 6,
+
+	// DVD 대여관리 메뉴
+	DVD_RENTAL_ADD = 1,
+	DVD_RENTAL_PRINT = 2,
+	DVD_RENTAL_RENEWAL = 3,
+	DVD_RENTAL_DELETE = 4,
+	DVD_RENTAL_MENU_RETURN = 5,
+	DVD_RENTAL_QUIT = 6,
+
+	// DVD 연체관리 메뉴
+	LATE_PAYMENT_ADD = 1,
+	LATE_PAYMENT_PRINT = 2,
+	LATE_PAYMENT_RENEWAL = 3,
+	LATE_PAYMENT_DELETE = 4,
+	LATE_PAYMENT_MENU_RETURN = 5,
+	LATE_PAYMENT_QUIT = 6,
+
+	// DVD 정보관리 메뉴
+	BLACK_LIST_ADD = 1,
+	BLACK_LIST_PRINT = 2,
+	BLACK_LIST_RENEWAL = 3,
+	BLACK_LIST_DELETE = 4,
+	BLACK_LIST_MENU_RETURN = 5,
+	BLACK_QUIT = 6,
+
+	YES = 1,
+	NO = 2
 };
 
-const int mainMenuOut = 7;
+// 변수 선언 부분
+const int menuCntOut = 7;
 int selectedMenu = 0;
 
-Person person;
-DvdInfo dvdInfo;
-RentalInfo rentalInfo;
-BlackList blackList;
+// 구조체 포인터 배열 선언 부분
+Person* perPtr[ARR_SIZE];
+DvdInfo* dvdPtr[ARR_SIZE];
+RentalInfo* rentalPtr[ARR_SIZE];
+BlackList* blackListPtr[ARR_SIZE];
 
 // 함수 선언 부분
 void showMainMenu();
 void showPersonInfoMenu();
+void showDvdInfoMenu();
+void showDvdRentalInfoMenu();
+void personInfoAdd();
+void showPersonPrint();
 
 void main() {
-	// 포인터 구조체 선언
-	struct Person* pArr;
-	struct DvdInfo* dvdArr;
-	struct RentalInfo* rentalArr;
-
 	// 메인메뉴 출력
+	showMainMenu();
+}
+
+void showMainMenu() {
 	while (1) {
-		showMainMenu();
+		printf("\n============================ \n");
+		printf("   DVD 대여 정보 프로그램 \n");
+		printf("============================ \n\n");
+
+		printf(" 1. 회원관리 \n");
+		printf(" 2. DVD 정보관리 \n");
+		printf(" 3. DVD 대여관리 \n");
+		printf(" 4. 연체 고객리스트 출력 \n");
+		printf(" 5. 블랙리스트 관리 \n");
+		printf(" 6. 종료 \n\n");
+
+		printf("선택 : ");
 
 		scanf("%d", &selectedMenu);
-		getchar();
+		getchar(); // 입력 버퍼 제거
 
 		if (selectedMenu == PERSON_INFO) {
-			printf("회원관리 기능 호출!\n");
-			getchar();
+			printf("회원관리 메뉴 호출! \n");
 			showPersonInfoMenu();
+			Sleep(1000);
 			break;
 		}
 		else if (selectedMenu == DVD_INFO) {
+			printf("DVD 정보관리 메뉴 호출! \n");
+			Sleep(1000);
+			showDvdInfoMenu();
 			break;
 		}
 		else if (selectedMenu == DVD_RENTAL) {
+			printf("DVD 대여관리 메뉴 호출! \n");
+			showDvdRentalInfoMenu();
 			break;
 		}
 		else if (selectedMenu == LATE_PAYMENT_PERSON) {
+			printf("DVD 연체관리 메뉴 호출! \n");
 			break;
 		}
 		else if (selectedMenu == BLACK_LIST) {
+			printf("블랙리스트 관리메뉴 호출! \n");
 			break;
 		}
 		else if (selectedMenu == QUIT) {
-			printf("프로그램을 종료합니다.\n");
+			printf("프로그램 종료합니다. \n");
 			break;
 		}
 
-		if (selectedMenu > mainMenuOut) {
+		if (selectedMenu > menuCntOut) {
 			printf("올바르지 않은 메뉴입니다. 다시 입력하세요. \n");
 		}
 	}
 }
 
-void showMainMenu() {
-	printf("\n============================ \n");
-	printf("   DVD 대여 정보 프로그램 \n");
-	printf("============================ \n\n");
+void showPersonInfoMenu() {
+	while (1) {
+		system("cls");
 
-	printf(" 1. 회원관리 \n");
-	printf(" 2. DVD 정보관리 \n");
-	printf(" 3. DVD 대여관리 \n");
-	printf(" 4. 연체 고객리스트 출력 \n");
-	printf(" 5. 블랙리스트 관리 \n");
-	printf(" 6. 종료 \n\n");
+		selectedMenu = 0; // selectedMenu 변수 초기화
 
-	printf("선택 : ");
+		printf("\n=========================== \n");
+		printf("        회원관리 \n");
+		printf("=========================== \n\n");
+
+		printf(" 1. 회원정보 추가 \n");
+		printf(" 2. 회원정보 출력 \n");
+		printf(" 3. 회원정보 수정 \n");
+		printf(" 4. 회원정보 삭제 \n");
+		printf(" 5. 뒤로가기 \n");
+		printf(" 6. 종료 \n\n");
+
+		printf("선택 : ");
+		scanf("%d", &selectedMenu);
+		getchar();
+
+		if (selectedMenu == PERSON_ADD) {
+			printf("회원추가 기능 호출! \n");
+			personInfoAdd();
+			break;
+		}
+		else if (selectedMenu == PERSON_PRINT) {
+			printf("회원정보 출력 기능 호출! \n");
+			showPersonPrint();
+			break;
+		}
+		else if (selectedMenu == PERSON_RENEWAL) {
+			printf("회원수정 기능 호출! \n");
+			Sleep(1000);
+			break;
+		}
+		else if (selectedMenu == PERSON_DELETE) {
+			printf("회원삭제 기능 호출! \n");
+			Sleep(1000);
+			break;
+		}
+		else if (selectedMenu == PERSON_MENU_RETURN) {
+			printf("메인메뉴 돌아가기 호출! \n");
+			Sleep(1000);
+			break;
+		}
+		else if (selectedMenu == PERSON_QUIT) {
+			printf("프로그램 종료합니다. \n");
+			break;
+		}
+
+		if (selectedMenu > menuCntOut) {
+			printf("잘못된 메뉴 선택입니다. 다시 입력하세요. \n");
+			Sleep(1000);
+		}
+	}
 }
 
-void showPersonInfoMenu() {
+void personInfoAdd() {
 	system("cls");
 
-	selectedMenu = 0; // selectedMenu 변수 초기화
+	// 포인터 구조체 배열 동적할당
+	for (int i = 0; i < sizeof(perPtr) / sizeof(Person*); i++) {
+		perPtr[i] = malloc(sizeof(Person));
+	}
 
-	printf("\n=========================== \n");
-	printf("        회원관리 \n");
-	printf("=========================== \n\n");
+	int cnt = 0, selectValue = 0;
 
-	printf(" 1. 회원정보 추가 \n");
-	printf(" 2. 회원정보 수정 \n");
-	printf(" 3. 회원정보 삭제 \n");
+	while (1) {
+		// 회원 이름
+		printf("\n%d번째 회원정보 입력 \n", cnt + 1);
+		printf("%d번째 회원 이름 입력 : ", cnt + 1);
+		gets(perPtr[cnt]->name);
+		getchar();
+		
+		// 회원 연락처
+		printf("%d번째 회원 연락처 입력 : ", cnt + 1);
+		gets(perPtr[cnt]->tel);
+		getchar();
+
+		// 연체횟수 0부터 시작
+		perPtr[cnt]->latePaymentCount = 0;
+
+		printf("계속 입력하시겠습니까? (1. 예), (2. 아니요) : ");
+		scanf("%d", &selectValue);
+		getchar();
+		
+		if (selectValue == YES) {
+			cnt++;
+		}
+		else if (selectValue == NO) {
+			showPersonInfoMenu(); // 전 메뉴로 돌아가기
+			break;
+		}
+	}
+
+	// 동적메모리 해제
+	for (int i = 0; i < sizeof(perPtr) / sizeof(Person*); i++) {
+		free(perPtr[i]);
+	}
+}
+
+void showPersonPrint() {
+	system("cls");
+
+	int cnt;
+
+	for (int i = 0; i < sizeof(perPtr) / sizeof(Person*); i++) {
+		if (perPtr[i] == NULL) {
+			printf("출력할 회원정보가 없습니다. \n");
+			Sleep(1000);
+			showPersonInfoMenu();
+			break;
+		}
+
+		printf("\n회원이름 : %s\n", perPtr[i]->name);
+		printf("연락처 : %s\n", perPtr[i]->tel);
+		printf("연체횟수 : %d\n", perPtr[i]->latePaymentCount);
+	}
+
+	printf("메뉴로 돌아가시려면 1을 입력하시고, 프로그램 종료하시려면 0을 입력해주세요. : ");
+	scanf("%d", &cnt);
+	getchar();
+
+	if (cnt == 1) {
+		showPersonInfoMenu();
+	}
+	else if (cnt == 0) {
+		printf("프로그램을 종료합니다. \n");
+		return 0;
+	}
+}
+
+void showDvdInfoMenu() {
+	while (1) {
+		system("cls");
+
+		selectedMenu = 0; // 변수 초기화
+
+		printf("\n============================ \n");
+		printf("       DVD 정보 관리 \n");
+		printf("============================ \n\n");
+
+		printf(" 1. DVD 정보 추가 \n");
+		printf(" 2. DVD 정보 수정 \n");
+		printf(" 3. DVD 정보 삭제 \n");
+		printf(" 4. 뒤로가기 \n");
+		printf(" 5. 종료 \n\n");
+
+		printf("선택 : ");
+		scanf("%d", &selectedMenu);
+		getchar();
+
+		if (selectedMenu == DVD_ADD) {
+			printf("DVD 정보 추가기능 호출! \n");
+			Sleep(1000);
+			break;
+		}
+		else if (selectedMenu == DVD_RENEWAL) {
+			printf("DVD 정보 수정기능 호출! \n");
+			Sleep(1000);
+			break;
+		}
+		else if (selectedMenu == DVD_DELETE) {
+			printf("DVD 정보 삭제기능 호출! \n");
+			Sleep(1000);
+			break;
+		}
+		else if (selectedMenu == DVD_MENU_RETURN) {
+			printf("메인메뉴 돌아가기 호출! \n");
+			Sleep(1000);
+			system("cls");
+			showMainMenu();
+			break;
+		}
+		else if (selectedMenu == DVD_QUIT) {
+			printf("프로그램을 종료합니다. \n");
+			break;
+		}
+
+		if (selectedMenu > menuCntOut) {
+			printf("잘못된 메뉴 선택입니다. 다시 입력하세요. \n");
+			Sleep(1000); // 1초 지연
+		}
+	}
+}
+
+void showDvdRentalInfoMenu() {
+	system("cls");
+
+	selectedMenu = 0; // 변수 초기화
+
+	printf("\n============================ \n");
+	printf("       DVD 대여 관리 \n");
+	printf("============================ \n\n");
+
+	printf(" 1. DVD 대여정보 추가 \n");
+	printf(" 2. DVD 대여정보 수정 \n");
+	printf(" 3. DVD 대여정보 삭제 \n");
 	printf(" 4. 뒤로가기 \n");
 	printf(" 5. 종료 \n\n");
 
 	printf("선택 : ");
-}
-
-void personAddInfo() {
-
+	scanf("%d", &selectedMenu);
+	getchar();
 }
